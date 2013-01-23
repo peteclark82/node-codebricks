3635497b664ed92ab5701ea4a93f3264a6c592bc
Pete Clark <pclarkgithub@gmail.com>
2013-01-23 21:36:33 +0000

    Added brick types
    # Added assetRoute brick type

---
+++ test/test_codebricks.types.brickTypes.js
@@ -0,0 +1,12 @@
+require("mocha");
+
+describe('User', function(){
+  describe('#save()', function(){
+    it('should save without error', function(done){
+     done();
+    })
+		it('should save without error', function(done){
+     done("hello");
+    })
+  })
+});
